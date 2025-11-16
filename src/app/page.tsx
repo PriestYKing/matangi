22975ebd19526ea4a2d4a6cb43b9d81e387ce9df
Yash Patidar <zoro@Yashs-MacBook-Air.local>
2025-11-16 12:48:10 +0530

    Added better auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
-import { getQueryClient, trpc } from "@/trpc/server";
-import { Client } from "./client";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+import { requireAuth } from "@/lib/auth-utils";
+import { caller } from "@/trpc/server";
 
 const Page = async () => {
-  const queryClient = getQueryClient();
+  await requireAuth();
 
-  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
+  const data = await caller.getUsers();
 
   return (
-    <div className="text-red-500">
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <Client />
-      </HydrationBoundary>
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+      Protected routes
+      {JSON.stringify(data)}
     </div>
   );
 };
